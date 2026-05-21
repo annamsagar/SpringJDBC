6dc5adea3577cfdb8f4a25b68690fab2d61521a8
annamsagar <annamsagar05@gmail.com>
2026-05-21 18:47:13 +0530

    linked with the h2 database with help of the Spring jdbc template

--- src/main/java/com/livein/JDBCEx/repository/StudentRepo.java
+++ src/main/java/com/livein/JDBCEx/repository/StudentRepo.java
@@ -1,6 +1,8 @@
 package com.livein.JDBCEx.repository;
 
 import com.livein.JDBCEx.model.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -9,13 +11,29 @@ import java.util.List;
 @Repository
 public class StudentRepo {
 
+    JdbcTemplate jdbc;
+
+    public JdbcTemplate getJdbc() {
+        return jdbc;
+    }
+
+    @Autowired
+    public void setJdbc(JdbcTemplate jdbc) {
+        this.jdbc = jdbc;
+    }
+
+
+    public void save(Student s1) {
+       String sql="insert into student (rollno,name,marks) values (?,?,?)";
+
+       int rows=jdbc.update(sql,s1.getRollno(),s1.getName(),s1.getMarks());
+        System.out.println(rows+" effected");
+    }
     public static List<Student> findAll() {
         List<Student> student= new ArrayList<>();
         return student;
     }
 
-    public void save(Student s1) {
-        System.out.println("saved student");
-    }
+
 
 }
